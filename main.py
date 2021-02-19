6b530ca86a4b9da2a5c3ed719606248f0230e249
arzkar <roguedevone@gmail.com>
2021-02-20 00:47:05 +0530

    Added search examples and updated pyproject.toml

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ def index():
     return {'API': 'Fanfiction Finder', 'Author': 'arzkar'}
 
 
+@app.get("/search/")
+def search():
+    return {'Search Examples': [
+        "['For ao3 works searching:','https://fanfictionapi.repl.co/search/ao3 Harry Potter and the Prince of Slytherin']",
+        "['For ao3 series searching:','https://fanfictionapi.repl.co/search/ao3 Harry Potter and the Prince of Slytherin series']",
+        "['For ffn story searching','https://fanfictionapi.repl.co/search/ffn cadmean victory']"
+    ]}
+
+
 @app.get("/search/{fic_name}")
 def get_fic(fic_name: str):
     res = find_fic(fic_name)
@@ -17,4 +26,4 @@ def get_fic(fic_name: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=5000)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000)
