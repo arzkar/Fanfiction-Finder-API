d4fb66a041bf50eb8cd6ffb569af241d603dba42
arzkar <arzkar.dev@gmail.com>
2021-06-07 18:36:28 +0530

    Major refactoring: Moved all the ffn & ao3 functions into their respective classes

---
+++ adapters/adapter_archiveofourown.py
@@ -0,0 +1,310 @@
+import requests
+import re
+from datetime import datetime
+from bs4 import BeautifulSoup
+from loguru import logger
+
+from utils.processing import get_ao3_series_works_index
+
+
+URL_VALIDATE = r"(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:/[^\s]*)?"
+
+
+class ArchiveOfOurOwn:
+    def __init__(self, BaseUrl):
+        self.BaseUrl = BaseUrl
+
+    def get_works_metadata(self):
+
+        if re.search(URL_VALIDATE, self.BaseUrl):
+
+            self.session = requests.Session()
+
+            response = self.session.get(self.BaseUrl)
+
+            logger.info(
+                f"Processing {self.BaseUrl} ")
+
+            ao3_soup = BeautifulSoup(response.content, 'html.parser')
+
+            try:
+                self.ao3_works_name = (ao3_soup.find(
+                    'h2', attrs={'class': 'title heading'}).contents[0]).strip()
+
+            except IndexError:  # Story Not Found
+                logger.error("ao3_works_name is missing.")
+                self.ao3_works_name = None
+                return
+
+            self.ao3_works_id = (re.search(r"\d+", self.BaseUrl)).group(0)
+
+            self.ao3_author_name = (ao3_soup.find(
+                'h3', attrs={'class': 'byline heading'})
+                .find('a').contents[0]).strip()
+
+            self.ao3_author_url = ao3_soup.find(
+                'h3', attrs={'class': 'byline heading'}).find('a', href=True)['href']
+
+            try:
+                self.ao3_works_summary = ao3_soup.find(
+                    'div', attrs={'class': 'summary module'}).find(
+                    'blockquote', attrs={'class': 'userstuff'}).text
+
+                # removing whitespaces
+                self.ao3_works_summary = re.sub(
+                    r'\s+', ' ', self.ao3_works_summary)
+
+            except AttributeError:  # if summary not found
+                self.ao3_works_summary = None
+
+            try:
+                self.ao3_works_status = (ao3_soup.find(
+                    'dl', attrs={'class': 'stats'}).find(
+                    'dt', attrs={'class': 'status'}).contents[0]).strip()
+
+                self.ao3_works_status = self.ao3_works_status.replace(":", "")
+
+            except AttributeError:  # if story status not found
+                self.ao3_works_status = None
+
+            try:
+                self.ao3_works_last_up = (ao3_soup.find(
+                    'dl', attrs={'class': 'stats'}).find(
+                    'dd', attrs={'class': 'status'}).contents[0]).strip()
+
+            except AttributeError:  # if story last updated not found
+                self.ao3_works_last_up = (ao3_soup.find(
+                    'dl', attrs={'class': 'stats'}).find(
+                    'dd', attrs={'class': 'published'}).contents[0]).strip()
+
+            self.ao3_works_published = (ao3_soup.find(
+                'dl', attrs={'class': 'stats'}).find(
+                'dd', attrs={'class': 'published'}).contents[0]).strip()
+
+            self.ao3_works_length = (ao3_soup.find(
+                'dl', attrs={'class': 'stats'}).find(
+                'dd', attrs={'class': 'words'}).contents[0]).strip()
+
+            self.ao3_works_chapters = (ao3_soup.find(
+                'dl', attrs={'class': 'stats'}).find(
+                'dd', attrs={'class': 'chapters'}).contents[0]).strip()
+
+            try:
+                self.ao3_works_rating = (ao3_soup.find(
+                    'dd', attrs={'class': 'rating tags'})
+                    .find('a').contents[0]).strip()
+
+            except AttributeError:
+                self.ao3_works_rating = None
+
+            self.ao3_works_fandom = (ao3_soup.find(
+                'dd', attrs={'class': 'fandom tags'})
+                .find('a').contents[0]).strip()
+
+            try:  # not found in every story
+                self.ao3_works_relationships = [
+                    a.contents[0].strip()
+                    for a in ao3_soup.find(
+                        'dd', attrs={'class': 'relationship tags'})
+                    .find_all('a')
+                ]
+                self.ao3_works_relationships = ", ".join(
+                    self.ao3_works_relationships)
+
+            except AttributeError:
+                self.ao3_works_relationships = None
+
+            try:  # not found in every story
+                self.ao3_works_characters = [
+                    a.contents[0].strip()
+                    for a in ao3_soup.find(
+                        'dd', attrs={'class': 'character tags'}).find_all('a')
+                ]
+
+                self.ao3_works_characters = ", ".join(
+                    self.ao3_works_characters)
+
+            except AttributeError:
+                self.ao3_works_characters = None
+
+            try:  # not found in every story
+                self.ao3_works_additional_tags = [
+                    a.contents[0].strip()
+                    for a in ao3_soup.find(
+                        'dd', attrs={'class': 'freeform tags'}).find_all('a')
+                ]
+
+                self.ao3_works_additional_tags = ", ".join(
+                    self.ao3_works_additional_tags)
+
+            except AttributeError:
+                self.ao3_works_additional_tags = None
+
+            self.ao3_works_language = (ao3_soup.find(
+                'dd', attrs={'class': 'language'}).contents[0]).strip()
+
+            try:
+                self.ao3_works_kudos = (ao3_soup.find(
+                    'dl', attrs={'class': 'stats'}).find(
+                    'dd', attrs={'class': 'kudos'}).contents[0]).strip()
+
+            except AttributeError:
+                self.ao3_works_kudos = None
+
+            try:
+                self.ao3_works_bookmarks = (ao3_soup.find(
+                    'dl', attrs={'class': 'stats'}).find(
+                    'dd', attrs={'class': 'bookmarks'})
+                    .find('a').contents[0]).strip()
+
+            except AttributeError:
+                self.ao3_works_bookmarks = None
+
+            try:
+                self.ao3_works_comments = (ao3_soup.find(
+                    'dl', attrs={'class': 'stats'}).find(
+                    'dd', attrs={'class': 'comments'}).contents[0]).strip()
+
+            except AttributeError:
+                self.ao3_works_comments = None
+
+            try:
+                self.ao3_works_hits = (ao3_soup.find(
+                    'dl', attrs={'class': 'stats'}).find(
+                    'dd', attrs={'class': 'hits'}).contents[0]).strip()
+
+            except AttributeError:
+                self.ao3_works_hits = None
+
+            self.ao3_works_warnings = (ao3_soup.find(
+                'dd', attrs={'class': 'warning tags'})
+                .find('a').contents[0]).strip()
+
+            self.ao3_works_category = (ao3_soup.find(
+                'dd', attrs={'class': 'category tags'})
+                .find('a').contents[0]).strip()
+
+            self.ao3_works_length = "{:,}".format(int(self.ao3_works_length))
+            self.ao3_works_chapters = re.search(
+                r"\d+", self.ao3_works_chapters).group(0)
+
+            self.ao3_author_url = "https://archiveofourown.org" \
+                + self.ao3_author_url
+
+            # remove everything after &sa from the BaseUrl
+            if re.search(r"^(.*?)&", self.BaseUrl) is not None:
+                self.BaseUrl = re.search(
+                    r"^(.*?)&", self.BaseUrl).group(1)
+
+        else:
+            logger.error("BaseUrl is invalid")
+
+    def get_series_metadata(self):
+
+        if re.search(URL_VALIDATE, self.BaseUrl):
+
+            self.session = requests.Session()
+
+            response = self.session.get(self.BaseUrl)
+
+            logger.info(
+                f"Processing {self.BaseUrl} ")
+
+            ao3_soup = BeautifulSoup(response.content, 'html.parser')
+
+            try:
+                self.ao3_series_name = (ao3_soup.find(
+                    'div', attrs={'class': 'series-show region'}).find(
+                    'h2', attrs={'class': 'heading'}).contents[0]).strip()
+
+            except IndexError:  # Series Not Found
+                logger.error("ao3_series_name is missing.")
+                self.ao3_series_name = None
+                return
+
+            self.ao3_series_id = (re.search(r"\d+", self.BaseUrl)).group(0)
+
+            self.ao3_author_name_list = ao3_soup.find(
+                'dl', attrs={'class': 'series meta group'}).find('dd') \
+                .find_all('a')
+
+            try:
+                self.ao3_series_summary = (ao3_soup.find(
+                    'div', attrs={'class': 'series-show region'}).find(
+                    'blockquote', attrs={'class': 'userstuff'}).text).strip()
+
+            except AttributeError:  # if summary not found
+                self.ao3_series_summary = None
+
+            try:
+                self.ao3_series_status = (ao3_soup.find(
+                    'dl', attrs={'class': 'stats'}).find(
+                    'dt', text='Complete:').findNext(
+                    'dd')).string.strip()
+
+                if self.ao3_series_status == "No":
+                    self.ao3_series_status = "In-Progress"
+
+                elif self.ao3_series_status == "Yes":
+                    self.ao3_series_status = "Completed"
+
+            except AttributeError:  # if story status not found
+                self.ao3_series_status = None
+
+            try:
+                self.ao3_series_last_up = ao3_soup.find(
+                    'div', attrs={'class': 'series-show region'}).find(
+                    'dt', text='Series Updated:').findNext(
+                    'dd').string.strip()
+
+            except AttributeError:  # if story last updated not found
+                self.ao3_series_last_up = ao3_soup.find(
+                    'div', attrs={'class': 'series-show region'}).find(
+                    'dt', text='Series Begun:').findNext(
+                    'dd').string.strip()
+
+            self.ao3_series_begun = ao3_soup.find(
+                'div', attrs={'class': 'series-show region'}).find(
+                'dt', text='Series Begun:').findNext(
+                'dd').string.strip()
+
+            try:
+                self.ao3_series_length = ao3_soup.find(
+                    'dt', text='Words:').findNext(
+                    'dd').string.strip()
+
+            except IndexError:  # Missing wordcount
+                self.ao3_series_length = None
+
+            self.ao3_series_works = ao3_soup.find(
+                'dt', text='Works:').findNext(
+                'dd').string.strip()
+
+            self.ao3_series_bookmarks = ao3_soup.find(
+                'dt', text='Bookmarks:').findNext(
+                'dd').string.strip()
+
+            self.ao3_series_fandom, self.ao3_series_works_index = \
+                get_ao3_series_works_index(
+                    ao3_soup)
+
+            self.ao3_author_name = []
+            for author in self.ao3_author_name_list:
+                self.ao3_author_name.append(author.string.strip())
+            self.ao3_author_name = ", ".join(self.ao3_author_name)
+
+            self.ao3_author_url = []
+            for author in self.ao3_author_name_list:
+                self.ao3_author_url.append(author['href'])
+            self.ao3_author_url = ", ".join(self.ao3_author_url)
+
+            self.ao3_author_url = "https://archiveofourown.org" \
+                + self.ao3_author_url
+
+            # remove everything after &sa from the BaseUrl
+            if re.search(r"^(.*?)&", self.BaseUrl) is not None:
+                self.BaseUrl = re.search(
+                    r"^(.*?)&", self.BaseUrl).group(1)
+
+        else:
+            logger.error("BaseUrl is invalid")
--- utils/processing.py
+++ utils/processing.py
@@ -2,138 +2,6 @@ from bs4 import BeautifulSoup
 import re
 
 
-def ao3_work_chapter_clean(ao3_work_chapters):
-    pos_of_slash1 = []
-    ao3_chapter = []
-    ao3_chapter_list = list(ao3_work_chapters)
-
-    for i in range(len(ao3_chapter_list)):
-        if "/" in ao3_chapter_list[i]:
-            pos_of_slash1.append(i)
-
-    if pos_of_slash1 is not None:
-        for i in range(0, pos_of_slash1[0]):
-            ao3_chapter.append(ao3_chapter_list[i])
-
-    ao3_chapter = ''.join(ao3_chapter)
-    return ao3_chapter
-
-
-def ffn_process_details(ffn_soup):
-    details = ffn_soup.find_all(
-        'span', {'class': 'xgray xcontrast_txt'}
-    )[0].text.split(' - ')
-
-    ffn_story_status, ffn_story_last_up, ffn_story_published = get_ffn_story_status(
-        ffn_soup, details)
-
-    ffn_story_reviews, ffn_story_favs, ffn_story_follows, ffn_story_rating = get_ffn_reviews_favs_follows(
-        details)
-
-    ffn_story_lang = details[1]
-    ffn_story_genre = details[2]
-    ffn_story_characters = details[3]
-
-    if re.search(r'\d', str(ffn_story_characters)) is not None:
-        ffn_story_characters = "Not Found"
-
-    ffn_story_length = get_ffn_word_cnt(details)
-    ffn_story_length = "{:,}".format(int(ffn_story_length))
-    ffn_story_chapters = str(get_ffn_chapters_cnt(details)).strip()
-
-    return ffn_story_status, ffn_story_last_up, ffn_story_published, ffn_story_length, ffn_story_chapters, ffn_story_reviews, ffn_story_favs, ffn_story_follows, ffn_story_rating, ffn_story_lang, ffn_story_genre, ffn_story_characters
-
-
-def get_ffn_story_status(ffn_soup, details):
-    dates = [date for date in ffn_soup.find_all(
-        'span') if date.has_attr('data-xutime')]
-
-    cnt = 0
-    for i in range(0, len(details)):
-        if details[i].startswith('Updated:'):
-            cnt = 1
-            ffn_story_last_up = str(date.fromtimestamp(
-                int(dates[0]['data-xutime'])))
-
-            ffn_story_published = str(date.fromtimestamp(
-                int(dates[1]['data-xutime'])))  # Published date
-
-            break  # if found, exit the loop to prevent overwriting of the variable
-
-        elif details[i].startswith('Published:'):
-            cnt = 2
-            ffn_story_last_up = str(date.fromtimestamp(
-                int(dates[0]['data-xutime'])))  # Published date
-
-            # if Updated not found, pub & last_up will be same
-            ffn_story_published = str(date.fromtimestamp(
-                int(dates[0]['data-xutime'])))  # Published date
-
-    if cnt == 1:
-        return "Not Completed", ffn_story_last_up, ffn_story_published
-    elif cnt == 2:
-        return "Completed", ffn_story_last_up, ffn_story_published
-
-
-def get_ffn_reviews_favs_follows(details):
-
-    for i in range(0, len(details)):
-        if details[i].startswith('Reviews:'):
-
-            ffn_story_reviews = details[i].replace('Reviews:', '').strip()
-
-            break  # if found, exit the loop to prevent overwriting of the variable
-
-        else:
-            ffn_story_reviews = 'Not found'
-
-    for i in range(0, len(details)):
-        if details[i].startswith('Favs:'):
-
-            ffn_story_favs = details[i].replace('Favs:', '').strip()
-
-            break  # if found, exit the loop to prevent overwriting of the variable
-
-        else:
-            ffn_story_favs = 'Not found'
-
-    for i in range(0, len(details)):
-        if details[i].startswith('Follows:'):
-
-            ffn_story_follow = details[i].replace('Follows:', '').strip()
-
-            break  # if found, exit the loop to prevent overwriting of the variable
-
-        else:
-            ffn_story_follow = 'Not found'
-
-    for i in range(0, len(details)):
-        if details[i].startswith('Rated:'):
-
-            ffn_story_rating = details[i].replace('Rated:', '').strip()
-
-            break  # if found, exit the loop to prevent overwriting of the variable
-
-        else:
-            ffn_story_rating = 'Not found'
-
-    return str(ffn_story_reviews), str(ffn_story_favs), str(ffn_story_follow), str(ffn_story_rating)
-
-
-def get_ffn_word_cnt(details):
-    search = [x for x in details if x.startswith("Words:")]
-    if len(search) == 0:
-        return 0
-    return int(search[0][len("Words:"):].replace(',', ''))
-
-
-def get_ffn_chapters_cnt(details):
-    search = [x for x in details if x.startswith("Chapters:")]
-    if len(search) == 0:
-        return 1  # 1 as the default chapter number
-    return int(search[0][len("Chapters:"):].replace(',', ''))
-
-
 def get_ao3_series_works_index(ao3_soup):
     ao3_series_works_html = []
     ao3_series_fandom_html = []
@@ -161,9 +29,9 @@ def get_ao3_series_works_index(ao3_soup):
         work_id = re.search(r"\d+", tag['href']).group(0)
 
         ao3_series_works_index.append({
-            'work_id': work_id,
-            'work': tag.text,
-            'work_url': "https://archiveofourown.org"+tag['href']
+            'works_id': work_id,
+            'works': tag.text,
+            'works_url': "https://archiveofourown.org"+tag['href']
         }
         )
 
